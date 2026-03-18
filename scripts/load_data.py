42b5b56e6d2ee0d55b37f68eb776f3bfd2c0f561
Faheem Usman <faheemrajwadkar@Faheems-MacBook-Pro.local>
2026-03-18 12:19:14 +0530

    Another change

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,7 +1,15 @@
 import duckdb
+import os
 
-DB_PATH = "./data/warehouse/heymax.duckdb"
+# Define paths
+DB_DIR = "./data/warehouse"
+DB_PATH = os.path.join(DB_DIR, "heymax.duckdb")
 CSV_PATH = "./data/raw/event_stream.csv"
+
+# Dynamic paths by ensuring it exists, or creating a new one
+if not os.path.exists(DB_DIR):
+    os.makedirs(DB_DIR)
+    print(f"Created directory: {DB_DIR}")
 
 conn = duckdb.connect(DB_PATH)
 
